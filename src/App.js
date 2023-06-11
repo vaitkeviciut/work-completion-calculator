8c290b862626901dc3a4c36b670a7932d86f61ed
vaitkeviciut <indre.vaitkeviciute96@gmail.com>
2023-06-11 18:47:45 +0300

    final commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path='/' element={<MainPage />} />
+        <Route path='/work-completion-calculator' element={<MainPage />} />
 
         <Route path='*' element={
           <div>
